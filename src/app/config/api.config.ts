120beadbb983824c0172465ac592629f23cc4cac
Shivam <shivam903975@gmail.com>
2026-05-01 19:31:59 +0530

    updated new dasboard and url changes

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -2,5 +2,5 @@
  * Global API Configuration
  * Update the `API_BASE_URL` when deploying to production.
  */
-export const API_BASE_URL = 'http://localhost:8000';
-// export const API_BASE_URL = 'https://app6.lmh-ai.in/gbtbackend';
+export const API_BASE_URL = 'https://app6.lmh-ai.in/gbtbackend';
+// export const API_BASE_URL = 'http://localhost:8000';
